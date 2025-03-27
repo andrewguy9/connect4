fffbd8418c571820c1ff92eaeb414dc9425f299d
Andrew Thomson <athomsonguy@gmail.com>
2025-03-27 14:02:48 -0700

    fixup! progress on getting tensors on the right devices.

--- connect.py
+++ connect.py
@@ -673,7 +673,8 @@ def train_supervised_vs_functions(net: Connect4Net,
                 total_moves += 1
 
                 # Compute the cross entropy loss using the trainer's action as target.
-                loss = F.cross_entropy(masked_logits.unsqueeze(0), torch.tensor([trainer_action]))
+                action_tensor = torch.Tensor([trainer_action]).to(device)
+                loss = F.cross_entropy(masked_logits.unsqueeze(0), action_tensor)
 
                 move_losses.append(loss)
                 
